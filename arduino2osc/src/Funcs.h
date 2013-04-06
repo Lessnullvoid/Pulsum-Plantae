05b6437b1075b872e806fb5ee148d5f5f0c02cf1
Thiago Hersan <thiago.hersan+github@gmail.com>
2013-04-06 01:36:05 -0700

    indented Funcs.h

--- arduino2osc/src/Funcs.h
+++ arduino2osc/src/Funcs.h
@@ -1,21 +1,22 @@
-    
-
-
 class aFunc: public Functor{
-public:
-    aFunc(ofPtr<Input> _input, ofPtr<ofxUISlider> _slider,ofPtr<PresetManager> _presetManager, ofPtr<OscSender>  _oscSender, int _index){
-        input = _input; slider = _slider; presetManager=_presetManager; oscSender=_oscSender; index=_index;
-    }
-    void execute() {
-        float val = input->getFloatValue();
-        float mapval = input->getMappedFloatValue();
-        slider->setValue(val);
-        oscSender->sendMessage( "/"+presetManager->getAddress( index ), mapval );
-    }
+ public:
+  aFunc(ofPtr<Input> _input, ofPtr<ofxUISlider> _slider,ofPtr<PresetManager> _presetManager, ofPtr<OscSender>  _oscSender, int _index){
+    input = _input; 
+    slider = _slider; 
+    presetManager=_presetManager; 
+    oscSender=_oscSender; 
+    index=_index;
+  }
+  void execute() {
+    float val = input->getFloatValue();
+    float mapval = input->getMappedFloatValue();
+    slider->setValue(val);
+    oscSender->sendMessage( "/"+presetManager->getAddress( index ), mapval );
+  }
     
-    int index;
-    ofPtr<Input> input;
-    ofPtr<ofxUISlider> slider;
-    ofPtr<OscSender> oscSender;
-    ofPtr<PresetManager> presetManager;
+  int index;
+  ofPtr<Input> input;
+  ofPtr<ofxUISlider> slider;
+  ofPtr<OscSender> oscSender;
+  ofPtr<PresetManager> presetManager;
 };
